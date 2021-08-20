2b9a62e8b6d122dad7d79e57bf4c9516888f32d5
bueryouth <62591673+bueryouth@users.noreply.github.com>
2021-08-20 09:34:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var logo = document.getElementById("myPopup");
+    logo.classList.toggle("show");
+}
+
+function myFunction1() {
+    var x = document.getElementById("snackbar")
+    x.className = "show";
+    setTimeout(function() {
+        x.className = x.className.replace("show", "");
+    }, 3000);
+}
